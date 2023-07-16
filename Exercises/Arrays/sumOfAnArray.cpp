b9e6b92a5f0cba6b3473c58f56f1d060dbe2748b
Decimo <marlonadiguemartin548@gmail.com>
2023-07-17 03:44:57 +0800

    sum of an array

--- Exercises/Arrays/sumOfAnArray.cpp
+++ Exercises/Arrays/sumOfAnArray.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 
-int main(){
+int sumOfTheArray(int arr[], int size){
   int sum = 0;
-  int arr[] = {1,2,3,4,5};
-
-  int size = sizeof(arr) / sizeof(arr[0]);
-
   int i;
-  for(i = 0; i < size;  i++){
+
+  for(int i = 0; i < size; i++){
     sum += arr[i];
   }
 
-  std::cout << sum << std::endl;
+  return sum;
+}
+
+int main(){
+  int sum = 0;
+  int arr[] = {1,2,3,4,5};
+  int size = sizeof(arr) / sizeof(arr[0]);
+
+  int result = sumOfTheArray(arr, size);
+
+  std::cout << result << std::endl;
 
   return 0;
+
 }
